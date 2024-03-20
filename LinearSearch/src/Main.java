2a67d31add1e6d7a379592f3cfefb9fa5bd62922
DuoMinhHieu <dmh6774@gmail.com>
2024-03-20 08:56:47 +0700

    main

--- LinearSearch/src/Main.java
+++ LinearSearch/src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int[] list = {2, 5, 8, 11, 45, 67, 79, 89, 90, 100};
-        System.out.println(linearSearch(list, 2));   /* 0 */
-        System.out.println(linearSearch(list, 11));  /* 3 */
-        System.out.println(linearSearch(list, 79));  /* 6 */
-        System.out.println(linearSearch(list, 1));   /* -1 */
-        System.out.println(linearSearch(list, 5));   /* 1 */
-        System.out.println(linearSearch(list, 80));  /* -1 */
-    }
-
-    private static Object linearSearch(int[] list, int i) {
-        return null;
+        System.out.println(LinearSearch.linearSearch(list, 2));
+        System.out.println(LinearSearch.linearSearch(list, 11));
+        System.out.println(LinearSearch.linearSearch(list, 79));
+        System.out.println(LinearSearch.linearSearch(list, 1));
+        System.out.println(LinearSearch.linearSearch(list, 5));
+        System.out.println(LinearSearch.linearSearch(list, 80));
     }
 }
